eb5a81b39bcddb49d053f33fb10a77b86e71fb81
amara <amara@AmaranathaReddy>
2024-09-24 00:44:40 +0800

    third commit

---
+++ PrintEvenNumbers.java
@@ -0,0 +1,17 @@
+package interview;
+
+public class PrintEvenNumbers {
+
+	public static void main(String[] args) {
+		int i=201;
+		while (i<500) {
+			
+			System.out.println(i);
+			i+=2;
+			
+		}
+	
+
+	}
+
+}
---
+++ ReverseStarProgram.java
@@ -0,0 +1,25 @@
+package interview;
+
+public class ReverseStarProgram {
+
+	public static void main(String[] args) {
+		
+		int i,j,n=5;
+		for( i =n;i>=0;i--)
+		{
+			
+			
+		for(j=0;j<=i;j++)
+			
+		
+		{
+		    System.out.print("*");
+		}
+		 System.out.println();
+		}
+	}
+}
+		
+
+	
+
