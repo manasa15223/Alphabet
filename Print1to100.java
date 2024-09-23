a35b40beb1d6f72214361f77b13899faebbaf2ff
amara <amara@AmaranathaReddy>
2024-09-24 00:35:25 +0800

    second commit

---
+++ Print1to100.java
@@ -0,0 +1,16 @@
+package interview;
+
+public class Print1to100 {
+
+	public static void main(String[] args) {
+		int i=1;
+		for(i=1;i<100;i++) {
+			System.out.println(i);
+			
+
+		}
+		
+
+	}
+
+}
